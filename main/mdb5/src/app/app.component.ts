b153fa1ab546d84e2b95e062efb84e2efc8be150
letrungnhan <19130157@st.hcmuaf.edu.vn>
2022-07-21 00:31:51 +0700

    add component home and about

--- main/mdb5/src/app/app.component.ts
+++ main/mdb5/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HomeComponent} from "./components/home/home.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
   title = 'mdb5-angular-ui-kit-pro-advanced';
 }
