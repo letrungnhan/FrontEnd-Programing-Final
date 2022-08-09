62af5c4188d6ec9deb50cb6daad26c5f0888b312
letrungnhan <80018213+letrungnhan@users.noreply.github.com>
2022-08-10 00:05:02 +0700

    +Update product page

--- main/MDB5/src/app/components/product/product.component.ts
+++ main/MDB5/src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  title: string = "Rating Star";
+  const = ["fas fa-star blue-text"];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
